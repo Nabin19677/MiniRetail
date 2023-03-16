bf42044367d7be89e11e54f349b1be555668a825
Anil Khadka <anil.khadka3777@gmail.com>
2023-03-16 17:46:25 +0545

    Fixed Detail Modal and Document View

--- agent-portal/src/utils/httpUtil.js
+++ agent-portal/src/utils/httpUtil.js
@@ -1,19 +1,19 @@
 import { httpBase } from './httpBaseUtil';
 
 export function fetch(endpoint, params) {
-  return httpBase().get(`/${endpoint}`, { params });
+  return httpBase().get(`/api/v1/${endpoint}`, { params });
 }
 
 
 export function store(endpoint, data) {
-  return httpBase().post(`/${endpoint}`, data);
+  return httpBase().post(`/api/v1/${endpoint}`, data);
 }
 
 
 export function update(endpoint, data) {
-  return httpBase().put(`/${endpoint}`, data);
+  return httpBase().put(`/api/v1/${endpoint}`, data);
 }
 
 export function destroy(endpoint, id) {
-  return httpBase().delete(`/${endpoint}/${id}`);
+  return httpBase().delete(`/api/v1/${endpoint}/${id}`);
 }
